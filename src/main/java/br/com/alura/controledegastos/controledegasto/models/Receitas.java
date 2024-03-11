41dbf63a97e4992fe7ee00ab8d3e985d9e5176c4
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-11 20:35:27 -0300

    feat: Criado os modelos de persistencia no banco de dados

---
+++ src/main/java/br/com/alura/controledegastos/controledegasto/models/Receitas.java
@@ -0,0 +1,87 @@
+package br.com.alura.controledegastos.controledegasto.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_receitas")
+public class Receitas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private Double valor;
+    private LocalDate data;
+
+    public Receitas() {
+    }
+
+    public Receitas(Long id, String descricao, Double valor, LocalDate data) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Double getValor() {
+        return valor;
+    }
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Receitas other = (Receitas) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Receitas [id=" + id + ", descricao=" + descricao + ", valor=" + valor + ", data=" + data + "]";
+    }
+    
+}
