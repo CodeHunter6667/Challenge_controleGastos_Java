af4bb018b8ea0fb03074edbe6c38261468bea0b4
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-11 20:21:03 -0300

    inicio do projeto

---
+++ src/main/java/br/com/alura/controledegastos/controledegasto/ControledegastoApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.controledegastos.controledegasto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledegastoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledegastoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/controledegastos/controledegasto/ControledegastoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.controledegastos.controledegasto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledegastoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
