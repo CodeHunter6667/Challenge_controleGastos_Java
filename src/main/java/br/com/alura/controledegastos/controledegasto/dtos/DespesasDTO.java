9258e1efcd92ee026b44f1531ee0dc760f7af345
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-13 20:16:31 -0300

    feat: Feito validacoes nas classes DTO

--- src/main/java/br/com/alura/controledegastos/controledegasto/dtos/DespesasDTO.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/dtos/DespesasDTO.java
@@ -3,8 +3,20 @@ package br.com.alura.controledegastos.controledegasto.dtos;
 import java.time.LocalDate;
 
 import br.com.alura.controledegastos.controledegasto.models.Despesas;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
-public record DespesasDTO(Long id, String descricao, Double valor, LocalDate data) {
+public record DespesasDTO(
+        Long id,
+        @NotBlank(message = "Descricao é obrigatória")
+        @Size(min = 5, max = 20, message = "Descricao deve conter entre 5 e 20 caracteres")
+        String descricao,
+        @Positive(message = "Valor não pode ser negativo ou zero")
+        @NotBlank(message = "Valor é obrigatório")
+        Double valor,
+        @NotBlank(message = "Data é obrigatória")
+        LocalDate data) {
     public DespesasDTO(Despesas entity) {
         this(entity.getId(), entity.getDescricao(), entity.getValor(), entity.getData());
     }   
--- src/main/java/br/com/alura/controledegastos/controledegasto/dtos/ReceitasDTO.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/dtos/ReceitasDTO.java
@@ -3,8 +3,20 @@ package br.com.alura.controledegastos.controledegasto.dtos;
 import java.time.LocalDate;
 
 import br.com.alura.controledegastos.controledegasto.models.Receitas;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
-public record ReceitasDTO(Long id, String descricao, Double valor, LocalDate data) {
+public record ReceitasDTO(
+        Long id,
+        @NotBlank(message = "Descricao é obrigatória")
+        @Size(min = 5, max = 20, message = "Descricao deve ter entre 5 e 20 caracteres")
+        String descricao,
+        @Positive(message = "Valor não pode ser zero ou negativo")
+        @NotBlank(message = "Valor é obrigatório")
+        Double valor,
+        @NotBlank(message = "Data é obrigatória")
+        LocalDate data) {
     public ReceitasDTO(Receitas entity) {
         this(entity.getId(), entity.getDescricao(), entity.getValor(), entity.getData());
     }
