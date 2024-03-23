d7fb183ed4b7b04eed523cb4bde7b64f5720514c
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-23 17:08:26 -0300

    feat: método repository para buscar por data

--- src/main/java/br/com/alura/controledegastos/controledegasto/repositories/ReceitasRepository.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/repositories/ReceitasRepository.java
@@ -1,10 +1,17 @@
 package br.com.alura.controledegastos.controledegasto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.controledegastos.controledegasto.models.Receitas;
 
+import java.util.List;
+
 @Repository
 public interface ReceitasRepository extends JpaRepository<Receitas, Long>{
+
+    @Query("SELECT obj FROM Receitas obj WHERE EXTRACT(MONTH FROM obj.data) = :mes " +
+            "AND EXTRACT(YEAR FROM obj.data) = :ano")
+    List<Receitas> searchByDate(Double mes, Double ano);
 }
