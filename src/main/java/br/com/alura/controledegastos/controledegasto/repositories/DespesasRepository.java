3ae85d7ca47ce82e3520f1b45bc396ea24c388b8
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-24 20:09:59 -0300

    feat: criado query para procurar por data

--- src/main/java/br/com/alura/controledegastos/controledegasto/repositories/DespesasRepository.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/repositories/DespesasRepository.java
@@ -1,10 +1,16 @@
 package br.com.alura.controledegastos.controledegasto.repositories;
 
+import br.com.alura.controledegastos.controledegasto.models.Despesas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import br.com.alura.controledegastos.controledegasto.models.Despesas;
+import java.util.List;
 
 @Repository
 public interface DespesasRepository extends JpaRepository<Despesas, Long>{
+
+    @Query("SELECT obj FROM Despesas obj WHERE EXTRACT(MONTH FROM obj.data) = :mes " +
+            "AND EXTRACT(YEAR FROM obj.data) = :ano")
+    List<Despesas> searchByDate(Double mes, Double ano);
 }
