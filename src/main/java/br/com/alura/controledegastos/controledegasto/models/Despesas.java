5a1f98abeb4b60e0eb3a84217318031b4ac64568
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-24 20:07:33 -0300

    feat: inclusao metodo Setter para Categorias

--- src/main/java/br/com/alura/controledegastos/controledegasto/models/Despesas.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/models/Despesas.java
@@ -66,6 +66,10 @@ public class Despesas {
         return categoria;
     }
 
+    public void setCategorias(Categorias categorias){
+        this.categoria = categorias;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
