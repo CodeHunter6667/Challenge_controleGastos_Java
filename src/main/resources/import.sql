f2f302fc8865aaf0d8cc2bdb2b6e909afa8bad51
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-24 20:27:26 -0300

    feat: script de inserts para Categorias

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Alimentação');
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Saúde');
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Moradia');
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Transporte');
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Educação');
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Lazer');
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Imprevistos');
+INSERT INTO tb_categorias(nomecategoria) VALUES ('Outras');
