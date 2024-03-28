ce8512bb7218059117078a427378d5985a9f60f5
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-28 14:53:13 -0300

    feat: Endpoint Get para buscar por nome

--- src/main/java/br/com/alura/controledegastos/controledegasto/controllers/ReceitasController.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/controllers/ReceitasController.java
@@ -1,23 +1,15 @@
 package br.com.alura.controledegastos.controledegasto.controllers;
 
-import java.net.URI;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import br.com.alura.controledegastos.controledegasto.dtos.ReceitasDTO;
 import br.com.alura.controledegastos.controledegasto.services.ReceitasService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/receitas")
@@ -41,6 +33,12 @@ public class ReceitasController {
     @GetMapping(value = "/{ano}/{mes}")
     public ResponseEntity<List<ReceitasDTO>> getByDate(@PathVariable Double mes,@PathVariable Double ano){
         List<ReceitasDTO> dto = service.searchByDate(mes, ano);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReceitasDTO>> getByName(@RequestParam(name = "nome", defaultValue = "") String nome){
+        List<ReceitasDTO> dto = service.getByName(nome);
         return ResponseEntity.ok().body(dto);
     }
 
