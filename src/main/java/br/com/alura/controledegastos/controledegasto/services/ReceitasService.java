7898ef10bccc6ded2800af768babc889e0b1824e
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-28 14:14:15 -0300

    feat: Servico de Receitas para buscar por nome

--- src/main/java/br/com/alura/controledegastos/controledegasto/services/ReceitasService.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/services/ReceitasService.java
@@ -1,17 +1,16 @@
 package br.com.alura.controledegastos.controledegasto.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 import br.com.alura.controledegastos.controledegasto.dtos.ReceitasDTO;
 import br.com.alura.controledegastos.controledegasto.models.Receitas;
 import br.com.alura.controledegastos.controledegasto.repositories.ReceitasRepository;
 import br.com.alura.controledegastos.controledegasto.services.exceptions.ReceitaNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ReceitasService {
@@ -35,6 +34,12 @@ public class ReceitasService {
     @Transactional(readOnly = true)
     public List<ReceitasDTO> searchByDate(Double mes, Double ano){
         List<Receitas> receitas = repository.searchByDate(mes, ano);
+        return receitas.stream().map(ReceitasDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReceitasDTO> getByName(String nome){
+        List<Receitas> receitas = repository.searchByName(nome);
         return receitas.stream().map(ReceitasDTO::new).toList();
     }
 
