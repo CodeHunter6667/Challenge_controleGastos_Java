97ea832a40907fc866692dc85e277d48b1ca2225
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-28 14:13:44 -0300

    feat: Query para buscar Receitas por nome

--- src/main/java/br/com/alura/controledegastos/controledegasto/repositories/ReceitasRepository.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/repositories/ReceitasRepository.java
@@ -1,10 +1,9 @@
 package br.com.alura.controledegastos.controledegasto.repositories;
 
+import br.com.alura.controledegastos.controledegasto.models.Receitas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import br.com.alura.controledegastos.controledegasto.models.Receitas;
 
 import java.util.List;
 
@@ -14,4 +13,7 @@ public interface ReceitasRepository extends JpaRepository<Receitas, Long>{
     @Query("SELECT obj FROM Receitas obj WHERE EXTRACT(MONTH FROM obj.data) = :mes " +
             "AND EXTRACT(YEAR FROM obj.data) = :ano")
     List<Receitas> searchByDate(Double mes, Double ano);
+
+    @Query("SELECT obj FROM Receitas obj WHERE UPPER(obj.nome) LIKE UPPER(:nome)")
+    List<Receitas> searchByName(String nome);
 }
