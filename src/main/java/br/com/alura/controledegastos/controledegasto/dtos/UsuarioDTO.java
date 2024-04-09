83f56949ae92b02825a3f72c12c6af6da59fb71e
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-04-09 11:43:35 -0300

    feat: criado DTO da entidade Usuario

---
+++ src/main/java/br/com/alura/controledegastos/controledegasto/dtos/UsuarioDTO.java
@@ -0,0 +1,19 @@
+package br.com.alura.controledegastos.controledegasto.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UsuarioDTO(Long id,
+                         @NotBlank(message = "O Login é obrigatório")
+                         @Size(min = 10, max = 25, message = "Login deve ter entre 10 e 25 caracteres")
+                         String login,
+                         @NotBlank(message = "A Senha é obrigatória")
+                         @Size(min = 5, max = 20, message = "Senha deve ter entre 5 e 20 caracteres")
+                         String senha,
+                         @NotBlank(message = "O Nome é obrigatório")
+                         String nome,
+                         @NotBlank(message = "O Email é obrigatório")
+                         @Email(message = "Email inválido")
+                         String email) {
+}
