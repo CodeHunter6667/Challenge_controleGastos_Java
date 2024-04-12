890253edfe608ec4ab1086d91e8fd386922c389a
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-04-12 17:33:48 -0300

    feat: mudança nas queries do repositório de Usuarios

--- src/main/java/br/com/alura/controledegastos/controledegasto/repositories/UsuarioRepository.java
+++ src/main/java/br/com/alura/controledegastos/controledegasto/repositories/UsuarioRepository.java
@@ -1,12 +1,19 @@
 package br.com.alura.controledegastos.controledegasto.repositories;
 
 import br.com.alura.controledegastos.controledegasto.models.Usuario;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    @Query("Select u From Usuario u Where u.login = :login")
-    Usuario searchUserByLogin(String login);
+    
+    UserDetails findByLogin(String login);
+    
+    @Query("SELECT u FROM Usuario u WHERE u.login = ?1")
+    Usuario procuraUserPorLogin(String login);
 }
